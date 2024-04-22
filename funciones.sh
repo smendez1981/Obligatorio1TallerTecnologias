8ce0939ec989d1fd4435a9c1b7aa2200bb7bb75d
smendez1981 <smendez1981@gmail.com>
2024-04-22 20:37:24 -0300

    Add files via upload
    Agrego opcion Algoritmo 1 y 2

--- funciones.sh
+++ funciones.sh
@@ -132,8 +132,9 @@ ControladorOpcionesMenu() {
         Algoritmo1
         ;;
     10)
-        echo "Has seleccionado la Opción Algoritmo 2"
-
+        echo "Opción Algoritmo 2"
+        echo "----------------------------------------"
+        Palindromo
         ;;
     x)
         echo "Saliendo..."
@@ -436,7 +437,7 @@ ConsultarDiccionario() {
 Algoritmo1() {
     echo ""
     read -p "Ingrese la cantidad de datos que desea ingresar: " cantidadDatos
- 
+
     # Verificar si la cantidad ingresada es un número positivo
     if ! [[ "$cantidadDatos" =~ ^[1-9][0-9]*$ ]]; then
         echo "Debe ingresar un número entero positivo."
@@ -452,7 +453,7 @@ Algoritmo1() {
 
         # Leer los datos ingresados por el usuario y calcular el menor y el mayor
         for ((i = 1; i <= "$cantidadDatos"; i++)); do
-            read -p  "Ingrese el dato $i: " dato           
+            read -p "Ingrese el dato $i: " dato
 
             # Verificar si el dato ingresado es un número entero
             if ! [[ $dato =~ ^[0-9]+$ ]]; then
@@ -465,16 +466,15 @@ Algoritmo1() {
             if [ -z "$menor" ] || [ "$dato" -lt "$menor" ]; then
                 menor=$dato
             fi
- 
+
             if [ -z "$mayor" ] || [ "$dato" -gt "$mayor" ]; then
                 mayor=$dato
             fi
 
-             #echo "Menor dato: ${menor}"
+            #echo "Menor dato: ${menor}"
             suma=$((suma + dato))
         done
- 
-         
+
         # Mostrar los resultados
         echo ""
         promedio=$(echo "scale=2; $suma / $cantidadDatos" | bc)
@@ -486,4 +486,31 @@ Algoritmo1() {
         MenuPrincipal
 
     fi
+}
+
+Palindromo() {
+    local palabra=""    
+    local palabrareversa=""
+
+    read -p "Ingrese la palabra $i: " palabra
+    local largo=${#palabra}
+
+    # Obtener el reverso de la palabra
+    for ((i = $largo - 1; i >= 0; i--)); do
+        palabrareversa="$palabrareversa${palabra:$i:1}"
+    done
+
+
+echo $palabrareversa
+echo $palabra
+    # Comparar la palabra original con su reverso
+    if [ "$palabra" == "$palabrareversa" ]; then
+        echo "La palabra \"$palabra\" es un palíndromo."
+    else
+        echo "La palabra \"$palabra\" no es un palíndromo."
+    fi
+
+    echo ""
+    read -p "Presiona Enter para continuar..."
+    MenuPrincipal
 }
